9cbcb2bc88e0db9ada818699e6283596f31fea8e
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2022-04-24 20:29:35 +0300

    Delete MovieApp directory

--- MovieApp/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import App from './components/App';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-
-ReactDOM.render(<App/>,document.getElementById('root'));
