fce4a6b448eceb66a2ea4b61d7d098c73d8335c7
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2022-08-12 09:29:37 +0300

    setup 2

--- Birthday-reminder/setup/src/App.js
+++ Birthday-reminder/setup/src/App.js
@@ -2,7 +2,15 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-    return <h2>reminder project setup</h2>;
+    return (
+        <main>
+            <section className='container'>
+                <h3>0 birthdays today</h3>
+                <List />
+                <button onClick={()=>console.log('you clicked me')}>Clear All</button>
+            </section>
+        </main>
+    );
 }
 
 export default App;
