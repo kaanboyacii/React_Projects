d8fe8c78ad0ef4b5a078454bbee2da42ad2a0540
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2022-08-17 13:18:19 +0300

    Setup 1, data and css added

--- menu/src/App.js
+++ menu/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Menu from './Menu';
+import Categories from './Categories';
+import items from './data';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
