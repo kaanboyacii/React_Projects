9220991d0c5fa7cf8794a7e15c143d70901d8dde
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2022-12-30 12:20:55 +0300

    Database mysql connection

--- Node.js-CRUD-mysql/backend/index.js
+++ Node.js-CRUD-mysql/backend/index.js
@@ -1,7 +1,27 @@
 import express from "express";
+import mysql from "mysql";
 
 const app = express()
 
-app.listen(8800, ()=>{
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "node-crud"
+});
+
+app.get("/", (req, res) => {
+    res.json("hello");
+});
+
+app.get("/books",(req, res)=>{
+    const q = "SELECT * FROM books"
+    db.query(q,(err,data)=>{
+        if(err) return res.json(err)
+        return res.json(data)
+    })
+});
+
+app.listen(8800, () => {
     console.log("Connected to backend")
 })
