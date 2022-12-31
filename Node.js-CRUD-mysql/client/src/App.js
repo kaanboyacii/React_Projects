0f0080767f7fa6d53729e505056a47b0493c1e15
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2022-12-31 11:13:14 +0300

    İnstall cors & axios, Book.jsx updated

--- Node.js-CRUD-mysql/client/src/App.js
+++ Node.js-CRUD-mysql/client/src/App.js
@@ -1,15 +1,20 @@
 import {
   BrowserRouter,
   Routes,
-  Routes,
+  Route,
 } from "react-router-dom";
+import Add from "./pages/Add";
+import Books from "./pages/Books";
+import Update from "./pages/Update";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element/>
+          <Route path="/" element={<Books/>} />
+          <Route path="/add" element={<Add/>} />
+          <Route path="/update" element={<Update/>} />
         </Routes>
       </BrowserRouter>
     </div>
--- Node.js-CRUD-mysql/client/src/pages/Books.jsx
+++ Node.js-CRUD-mysql/client/src/pages/Books.jsx
@@ -1,6 +1,22 @@
 import React from 'react'
+import { useEffect } from 'react';
+import { useState } from 'react'
+import axios from 'axios'
 
 const Books = () => {
+    const [books,setBooks] = useState([]);
+
+    useEffect(()=>{
+        const fetchAllBooks = async () =>{
+            try {
+                const res = await axios.get("http://localhost:8800/books");
+                console.log(res);
+            } catch (err) {
+                console.log(err)
+            }
+        }
+        fetchAllBooks();
+    },[])
   return (
     <div>Books</div>
   )
