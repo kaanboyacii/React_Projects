94761efd0065230c260d0d609613c40925af5f10
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2022-12-31 11:05:35 +0300

    Router doom & pages created

--- Node.js-CRUD-mysql/client/src/App.js
+++ Node.js-CRUD-mysql/client/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Routes,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ Node.js-CRUD-mysql/client/src/pages/Update.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Update = () => {
+  return (
+    <div>Update</div>
+  )
+}
+
+export default Update
