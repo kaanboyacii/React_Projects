4add19e656e846bbc4c3ce1d849fd6c484c1dbc7
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-01-02 10:14:37 +0300

    Delet Item from db

--- Node.js-CRUD-mysql/client/src/pages/Books.jsx
+++ Node.js-CRUD-mysql/client/src/pages/Books.jsx
@@ -1,41 +1,54 @@
-import React from 'react'
-import { useEffect } from 'react';
-import { useState } from 'react'
-import axios from 'axios'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 const Books = () => {
-    const [books,setBooks] = useState([]);
+  const [books, setBooks] = useState([]);
 
-    useEffect(()=>{
-        const fetchAllBooks = async () =>{
-            try {
-                const res = await axios.get("http://localhost:8800/books");
-                setBooks(res.data);
-            } catch (err) {
-                console.log(err)
-            }
-        }
-        fetchAllBooks();
-    },[])
+  useEffect(() => {
+    const fetchAllBooks = async () => {
+      try {
+        const res = await axios.get("http://localhost:8800/books");
+        setBooks(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchAllBooks();
+  }, []);
+
+  const handleDelete = async (id) => {
+    try {
+      await axios.delete("http://localhost:8800/books/" + id);
+      window.location.reload();
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
-      <div>
-        <h1>Kaan Book Shop</h1>
-        <div className="books">
-            {books.map((books)=>(
-                <div className="book" key={books.id}>
-                    {books.cover && <img src={books.cover} alt="" />}
-                    <h2>{books.title}</h2>
-                    <p>{books.desc}</p>
-                    <span>{books.price}</span>
-                    <button className='delete'>Delete</button>
-                    <button className='update'>Update</button>
-                </div>
-            ))}
-        </div>
-        <button><Link to="/add" > Add new book</Link></button>
+    <div>
+      <h1>Kaan Book Shop</h1>
+      <div className="books">
+        {books.map((books) => (
+          <div className="book" key={books.id}>
+            {books.cover && <img src={books.cover} alt="" />}
+            <h2>{books.title}</h2>
+            <p>{books.desc}</p>
+            <span>{books.price}</span>
+            <button className="delete" onClick={() => handleDelete(books.id)}>
+              Delete
+            </button>
+            <button className="update">Update</button>
+          </div>
+        ))}
+      </div>
+      <button>
+        <Link to="/add"> Add new book</Link>
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Books
+export default Books;
