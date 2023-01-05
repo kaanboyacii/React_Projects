2c1d41c4b6e7c71d3e2e116a184d746996b06f7b
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-01-05 12:36:23 +0300

    İnstall project

--- BlogApp/client/src/index.js
+++ BlogApp/client/src/index.js
@@ -1,13 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { AuthContexProvider } from "./context/authContext";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AuthContexProvider>
-      <App />
-    </AuthContexProvider>
+    <App />
   </React.StrictMode>
 );
