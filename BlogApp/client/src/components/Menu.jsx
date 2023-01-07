2efbf1b3ff5bcce8d17468ae16d51f876ea0a8d9
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-01-07 15:10:37 +0300

    Single post page design

---
+++ BlogApp/client/src/components/Menu.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+function Menu() {
+  const posts = [
+    {
+      id: 1,
+      title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+      desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
+      img: "https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    },
+    {
+      id: 2,
+      title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+      desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
+      img: "https://images.pexels.com/photos/6489663/pexels-photo-6489663.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    },
+    {
+      id: 3,
+      title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+      desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
+      img: "https://images.pexels.com/photos/4230630/pexels-photo-4230630.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    },
+    {
+      id: 4,
+      title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+      desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
+      img: "https://images.pexels.com/photos/6157049/pexels-photo-6157049.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    },
+  ];
+
+  return (
+    <div className="menu">
+      <h1>Other posts you may like</h1>
+      {posts.map((post) => (
+        <div className="post" key={post.id}>
+          <img src={post.img} alt="" />
+          <h2>{post.title}</h2>
+          <button>Read More</button>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Menu;
--- BlogApp/client/src/pages/Single.jsx
+++ BlogApp/client/src/pages/Single.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from "react";
+import Edit from "../img/edit.png";
+import Delete from "../img/delete.png";
+import { Link } from "react-router-dom";
+import Menu from "../components/Menu";
 
 const Single = () => {
   return (
-    <div>Single</div>
-  )
-}
+    <div className="single">
+      <div className="content">
+        <img
+          src="https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          alt=""
+        />
+        <div className="user">
+          <img
+            src="https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+          />
+          <div className="info">
+            <span>Kaan</span>
+            <p>Posted 2 days ago</p>
+          </div>
+          <div className="edit">
+            <Link to={`/write?edit=2`}>
+              <img src={Edit} alt="" />
+            </Link>
+            <img src={Delete} alt="" />
+          </div>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia
+            sapiente eligendi explicabo assumenda velit quisquam facilis sint,
+            similique, nulla fugiat molestias provident recusandae architecto,
+            illum impedit. Eos dolore similique nulla voluptate, at voluptates
+            ex. Quae animi voluptatibus a quis quidem, labore magnam veniam
+            molestias autem temporibus nisi, omnis porro sed excepturi?
+            Consectetur velit assumenda quam dicta, expedita rerum voluptatibus
+            eius sunt voluptatum hic eveniet, vitae non. Quos asperiores tempora
+            laudantium possimus dolore officiis reiciendis? Velit, explicabo.
+            Nostrum molestias impedit blanditiis!
+          </p>
+        </div>
+      </div>
+      <Menu />
+    </div>
+  );
+};
 
-export default Single
+export default Single;
