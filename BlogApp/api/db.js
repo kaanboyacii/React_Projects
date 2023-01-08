742b45461b83127e54f240d6eff0af9ba5fc6b6d
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-01-09 00:04:52 +0300

    Created controllers and routes

--- BlogApp/api/db.js
+++ BlogApp/api/db.js
@@ -4,5 +4,5 @@ export const db = mysql.createConnection({
     host:"localhost",
     user:"root",
     password:"",
-    database:"BlogAppNode"
+    database:"BlogApp"
 });
--- BlogApp/api/index.js
+++ BlogApp/api/index.js
@@ -1,8 +1,10 @@
 import express from "express";
+import postRoutes from "./routes/posts.js";
 
 const app = express();
 
 app.use(express.json());
+app.use("/api/posts",postRoutes);
 
 
 app.listen(8800,()=>{
