4bf1bbd068141e7b870466f88f6d8c8a00a9f4a0
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-03-02 19:57:33 +0300

    Realtime Chat Login page

--- Realtime-Chat/src/pages/Login.jsx
+++ Realtime-Chat/src/pages/Login.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import Add from "../img/addAvatar.png"
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='formContainer'>
+        <div className="formWrapper">
+          <span className='logo'>Pintor Chat</span>
+          <span className='title'>Login</span>
+            <form action="">
+                <input type="email" placeholder='display email' />
+                <input type="password" placeholder='display password' />
+                <button>Login</button>
+            </form>
+            <p>You don't have an account ? Register</p>
+        </div>
+    </div>
   )
 }
 
