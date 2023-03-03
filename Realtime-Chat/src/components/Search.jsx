402aed68749688d73e6a366c718a05e30344d1c8
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-03-03 13:56:07 +0300

    Realtime Chat Design 2

--- Realtime-Chat/src/components/Search.jsx
+++ Realtime-Chat/src/components/Search.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const Search = () => {
   return (
-    <div>Search</div>
-  )
-}
+    <div className="search">
+      <div className="searchForm">
+        <input type="text" placeholder="Find a user" />
+      </div>
+      <div className="userChat">
+        <img src="https://miro.medium.com/max/1400/1*coAerR-bUiPH8l55CUZ4Kw.jpeg" />
+        <div className="userChatInfo">
+          <span>Jane</span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Search
+export default Search;
--- Realtime-Chat/src/components/Sidebar.jsx
+++ Realtime-Chat/src/components/Sidebar.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import Chats from './Chats'
 import Navbar from './Navbar'
+import Search from './Search'
 
 const Sidebar = () => {
   return (
     <div className='sidebar'>
         <Navbar/>
+        <Search/>
+        <Chats/>
     </div>
   )
 }
