c9344c5f1876c48be768391dd16a31baee02aecd
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-03-04 23:56:46 +0300

    Realtime Chat protected route

--- Realtime-Chat/src/App.js
+++ Realtime-Chat/src/App.js
@@ -7,13 +7,28 @@ import { useContext } from 'react';
 import { AuthContext } from './context/AuthContext';
 
 function App() {
-  const {currentUser} = useContext(AuthContext)
-  console.log(currentUser)
+  const { currentUser } = useContext(AuthContext);
+
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />;
+    }
+
+    return children
+  };
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/">
-          <Route index element={<Home />} />
+          <Route
+            index
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
           <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
         </Route>
