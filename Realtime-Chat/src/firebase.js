d277f83ef8005d2891cf21c5be844ed49608383c
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-03-04 23:21:48 +0300

    Realtime Chat firebase v9 Authentication 1

--- Realtime-Chat/src/firebase.js
+++ Realtime-Chat/src/firebase.js
@@ -1,19 +1,22 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
+import { getStorage, ref } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBOzF6z1-WpL0PFzGvmRkOj9RiU-l9W5Q0",
-  authDomain: "chat-2b654.firebaseapp.com",
-  projectId: "chat-2b654",
-  storageBucket: "chat-2b654.appspot.com",
-  messagingSenderId: "33561745762",
-  appId: "1:33561745762:web:e87a8048f2627914af743b",
-  measurementId: "G-F7XGPW0SFZ"
+    apiKey: "AIzaSyBOzF6z1-WpL0PFzGvmRkOj9RiU-l9W5Q0",
+    authDomain: "chat-2b654.firebaseapp.com",
+    projectId: "chat-2b654",
+    storageBucket: "chat-2b654.appspot.com",
+    messagingSenderId: "33561745762",
+    appId: "1:33561745762:web:e87a8048f2627914af743b",
+    measurementId: "G-F7XGPW0SFZ"
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 export const auth = getAuth()
+export const storage = getStorage();
+export const db = getFirestore();
+
--- Realtime-Chat/src/pages/Register.jsx
+++ Realtime-Chat/src/pages/Register.jsx
@@ -1,27 +1,69 @@
-import React from 'react'
-import Add from "../img/addAvatar.png"
+import React, { useState } from "react";
+import Add from "../img/addAvatar.png";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth, db, storage } from "../firebase";
+import { async } from "@firebase/util";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { doc, setDoc } from "firebase/firestore";
 
 const Register = () => {
-  return (
-    <div className='formContainer'>
-        <div className="formWrapper">
-          <span className='logo'>Pintor Chat</span>
-          <span className='title'>Register</span>
-            <form action="">
-                <input type="text" placeholder='display name' />
-                <input type="email" placeholder='display email' />
-                <input type="password" placeholder='display password' />
-                <input style={{display:"none"}} type="file" id='file'/>
-                <label htmlFor="file">
-                  <img src={Add} alt="" />
-                  <span>Add an avatar</span>
-                </label>
-                <button>Sign up</button>
-            </form>
-            <p>You do have an account ? Login</p>
-        </div>
-    </div>
-  )
-}
+  const [err, setErr] = useState(false);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const displayName = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+    const file = e.target[3].files[0];
 
-export default Register
+    try {
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+      const storageRef = ref(storage, displayName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on(
+        (error) => {
+          setErr(true);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+            await updateProfile(res.user, {
+              displayName,
+              photoURL: downloadURL,
+            });
+            await setDoc(doc(db, "users", res.user.uid), {
+              uid: res.user.uid,
+              displayName,
+              email,
+              photoURL: downloadURL,
+            });
+          });
+        }
+      );
+    } catch (err) {
+      setErr(true);
+    }
+  };
+
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">Pintor Chat</span>
+        <span className="title">Register</span>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="display name" />
+          <input type="email" placeholder="display email" />
+          <input type="password" placeholder="display password" />
+          <input style={{ display: "none" }} type="file" id="file" />
+          <label htmlFor="file">
+            <img src={Add} alt="" />
+            <span>Add an avatar</span>
+          </label>
+          <button>Sign up</button>
+          {err && <span>Something went wrong!</span>}
+        </form>
+        <p>You do have an account ? Login</p>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
