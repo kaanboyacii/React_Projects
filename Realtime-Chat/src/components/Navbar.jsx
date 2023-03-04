6a9335b15b385c24f45da236ab2ce638c98ea4de
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-03-05 00:12:22 +0300

    Realtime Chat Login with email and password

--- Realtime-Chat/src/components/Navbar.jsx
+++ Realtime-Chat/src/components/Navbar.jsx
@@ -1,18 +1,20 @@
-import { signOut } from 'firebase/auth'
-import React from 'react'
-import { auth } from '../firebase'
+import { signOut } from "firebase/auth";
+import React, { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { auth } from "../firebase";
 
 const Navbar = () => {
+  const { currentUser } = useContext(AuthContext);
   return (
-    <div className='navbar'>
-        <span className='logo'>Pintor Chat</span>
-        <div className="user">
-            <img src="https://miro.medium.com/max/1400/1*coAerR-bUiPH8l55CUZ4Kw.jpeg" alt="" />
-            <span>Jack</span>
-            <button onClick={()=>signOut(auth)}>Logout</button>
-        </div>
+    <div className="navbar">
+      <span className="logo">Lama Chat</span>
+      <div className="user">
+        <img src={currentUser.photoURL} alt="" />
+        <span>{currentUser.displayName}</span>
+        <button onClick={() => signOut(auth)}>logout</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- Realtime-Chat/src/pages/Login.jsx
+++ Realtime-Chat/src/pages/Login.jsx
@@ -1,21 +1,39 @@
-import React from 'react'
-import Add from "../img/addAvatar.png"
+import React, { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
 
 const Login = () => {
-  return (
-    <div className='formContainer'>
-        <div className="formWrapper">
-          <span className='logo'>Pintor Chat</span>
-          <span className='title'>Login</span>
-            <form action="">
-                <input type="email" placeholder='display email' />
-                <input type="password" placeholder='display password' />
-                <button>Login</button>
-            </form>
-            <p>You don't have an account ? Register</p>
-        </div>
-    </div>
-  )
-}
+  const [err, setErr] = useState(false);
+  const navigate = useNavigate();
 
-export default Login
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate("/")
+    } catch (err) {
+      setErr(true);
+    }
+  };
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">Lama Chat</span>
+        <span className="title">Login</span>
+        <form onSubmit={handleSubmit}>
+          <input type="email" placeholder="email" />
+          <input type="password" placeholder="password" />
+          <button>Sign in</button>
+          {err && <span>Something went wrong</span>}
+        </form>
+        <p>You don't have an account? <Link to="/register">Register</Link></p>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
