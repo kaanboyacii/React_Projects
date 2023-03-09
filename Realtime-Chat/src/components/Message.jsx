226d95907fb74af32669fe88338931653f1dd824
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-03-09 13:51:11 +0300

    Send realtime-chat message

--- Realtime-Chat/src/components/Message.jsx
+++ Realtime-Chat/src/components/Message.jsx
@@ -1,23 +1,39 @@
-import React, { useContext } from 'react'
-import { AuthContext } from '../context/AuthContext'
-import { ChatContext } from '../context/ChatContext'
+import React, { useContext, useEffect, useRef } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { ChatContext } from "../context/ChatContext";
 
-const Message = ({message}) => {
-  const {currentUser} = useContext(AuthContext)
-  const {data} = useContext(ChatContext)
+const Message = ({ message }) => {
+  const { currentUser } = useContext(AuthContext);
+  const { data } = useContext(ChatContext);
+
+  const ref = useRef();
+
+  useEffect(() => {
+    ref.current?.scrollIntoView({ behavior: "smooth" });
+  }, [message]);
 
   return (
-    <div className='message owner'>
-      {/* <div className="messageInfo">
-        <img src="https://images.pexels.com/photos/4519122/pexels-photo-4519122.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+    <div
+      ref={ref}
+      className={`message ${message.senderId === currentUser.uid && "owner"}`}
+    >
+      <div className="messageInfo">
+        <img
+          src={
+            message.senderId === currentUser.uid
+              ? currentUser.photoURL
+              : data.user.photoURL
+          }
+          alt=""
+        />
         <span>just now</span>
       </div>
       <div className="messageContent">
-        <p>hello</p>
-        <img src="https://images.pexels.com/photos/886521/pexels-photo-886521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
-      </div> */}
+        <p>{message.text}</p>
+        {message.img && <img src={message.img} alt="" />}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Message
+export default Message;
