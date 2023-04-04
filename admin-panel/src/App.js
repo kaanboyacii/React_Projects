cb8aba5fc5ccfabf27a77d7b9a6d73c9328cc242
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-04-04 13:44:52 +0300

    Admin Panel folder setup 2

--- admin-panel/src/App.js
+++ admin-panel/src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
+import Home from "./pages/home/Home";
 
 function App() {
   return (
     <div className="App">
-      Lama dev
+      <Home/>
     </div>
   );
 }
---
+++ admin-panel/src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./home.scss"
+
+const Home = () => {
+  return (
+    <div className='home'>
+        <h1 className='title'>Lama Dev</h1>
+    </div>
+  )
+}
+
+export default Home
