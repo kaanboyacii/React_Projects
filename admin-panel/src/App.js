01eec859a93cdefc1a6c246a0d672b158dfdad9b
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-04-05 12:15:29 +0300

    React-router-dom install

--- admin-panel/src/App.js
+++ admin-panel/src/App.js
@@ -1,9 +1,22 @@
 import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import List from "./pages/list/List";
+import Single from "./pages/single/Single";
+import New from "./pages/new/New";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { render } from "react"
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home/>}/>
+            <Route path="login" element={<Login/>}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
