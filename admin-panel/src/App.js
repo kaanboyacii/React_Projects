dcd3825a00e2fcfa5003dfb76aab06e8c39ddc80
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-04-05 12:24:04 +0300

    routes

--- admin-panel/src/App.js
+++ admin-panel/src/App.js
@@ -4,7 +4,6 @@ import List from "./pages/list/List";
 import Single from "./pages/single/Single";
 import New from "./pages/new/New";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { render } from "react"
 
 function App() {
   return (
@@ -14,6 +13,16 @@ function App() {
           <Route path="/">
             <Route index element={<Home/>}/>
             <Route path="login" element={<Login/>}/>
+            <Route path="users"> 
+              <Route index element={<List/>} />
+              <Route path=":userId" element={<Single/>} />
+              <Route path="new" element={<New/>} />
+            </Route>
+            <Route path="products"> 
+              <Route index element={<List/>} />
+              <Route path=":productId" element={<Single/>} />
+              <Route path="new" element={<New/>} />
+            </Route>
           </Route>
         </Routes>
       </BrowserRouter>
