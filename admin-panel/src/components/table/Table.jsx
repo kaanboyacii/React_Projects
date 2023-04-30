61fcf4563223e8b0ca181b7ab53edff0cfd1827d
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-04-30 10:54:47 +0300

    Admin Panel MUI table p1

---
+++ admin-panel/src/components/table/Table.jsx
@@ -0,0 +1,9 @@
+import "./table.scss"
+
+const Table = () => {
+  return (
+    <div className="table">Table</div>
+  )
+}
+
+export default Table
--- admin-panel/src/pages/home/Home.jsx
+++ admin-panel/src/pages/home/Home.jsx
@@ -5,6 +5,7 @@ import Navbar from "../../components/navbar/Navbar";
 import Widget from "../../components/widget/Widget";
 import Featured from "../../components/featured/Featured";
 import Chart from "../../components/chart/Chart";
+import Table from "../../components/table/Table";
 
 const Home = () => {
   return (
@@ -13,14 +14,18 @@ const Home = () => {
       <div className="homeContainer">
         <Navbar />
         <div className="widgets">
-          <Widget type="user"/>
-          <Widget type="order"/>
-          <Widget type="earning"/>
-          <Widget type="balance"/>
+          <Widget type="user" />
+          <Widget type="order" />
+          <Widget type="earning" />
+          <Widget type="balance" />
         </div>
         <div className="charts">
-          <Featured/>
-          <Chart/>
+          <Featured />
+          <Chart />
+        </div>
+        <div className="listContainer">
+          <div className="listTitle">Latest Transactions</div>
+          <Table />
         </div>
       </div>
     </div>
