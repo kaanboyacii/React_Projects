c54e27859a04f3ec9f219af331c8d89cf912572b
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-05-01 09:31:30 +0300

    Admin Panel MUI Item page

--- admin-panel/src/components/chart/Chart.jsx
+++ admin-panel/src/components/chart/Chart.jsx
@@ -17,11 +17,11 @@ const data = [
   { name: "June", Total: 1700 },
 ];
 
-const Chart = ({ }) => {
+const Chart = ({ aspect, title }) => {
   return (
     <div className="chart">
-      <div className="title">Last 6 months (Revenue)</div>
-      <ResponsiveContainer width="100%" aspect={2/1}>
+      <div className="title">{title}</div>
+      <ResponsiveContainer width="100%" aspect={aspect}>
         <AreaChart
           width={730}
           height={250}
