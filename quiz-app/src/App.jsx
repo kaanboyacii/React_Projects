fde9e8ecb225664aefa8728cd42ed4f5443989bd
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-05-05 10:27:52 +0300

    quiz-app design main container

--- quiz-app/src/App.jsx
+++ quiz-app/src/App.jsx
@@ -27,7 +27,12 @@ function App() {
 
   return (
     <div className="app">
-      <div className="main">main</div>
+      <div className="main">
+        <div className="top">
+          <div className="timer">30</div>
+        </div>
+        <div className="bottom">question and answers</div>
+      </div>
       <div className="pyramid">
         <ul className="moneyList">
           {moneyPyramid.map((m) => (
