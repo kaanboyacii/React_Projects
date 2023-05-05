fdaa9e7c05f0ff390038d8d24ab8d58a939a128b
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-05-05 10:16:51 +0300

    quiz-app react design p2

--- quiz-app/src/App.jsx
+++ quiz-app/src/App.jsx
@@ -1,16 +1,40 @@
-import "./app.css"
+import { useMemo } from "react";
+import "./app.css";
 
 function App() {
+  const moneyPyramid = useMemo(
+    () =>
+      [
+        { id: 1, amount: "$ 100" },
+        { id: 2, amount: "$ 200" },
+        { id: 3, amount: "$ 300" },
+        { id: 4, amount: "$ 500" },
+        { id: 5, amount: "$ 1.000" },
+        { id: 6, amount: "$ 2.000" },
+        { id: 7, amount: "$ 4.000" },
+        { id: 8, amount: "$ 8.000" },
+        { id: 9, amount: "$ 16.000" },
+        { id: 10, amount: "$ 32.000" },
+        { id: 11, amount: "$ 64.000" },
+        { id: 12, amount: "$ 125.000" },
+        { id: 13, amount: "$ 250.000" },
+        { id: 14, amount: "$ 500.000" },
+        { id: 15, amount: "$ 1.000.000" },
+      ].reverse(),
+    []
+  );
+
   return (
     <div className="app">
       <div className="main">main</div>
       <div className="pyramid">
         <ul className="moneyList">
-          <li className="moneyListItem">$ 500</li>
-          <li className="moneyListItem">$ 400</li>
-          <li className="moneyListItem">$ 300</li>
-          <li className="moneyListItem">$ 200</li>
-          <li className="moneyListItem">$ 100</li>
+          {moneyPyramid.map((m) => (
+          <li className="moneyListItem">
+            <span className="moneyListItemNumber">{m.id}</span>
+            <span className="moneyListItemAmount">{m.amount}</span>
+          </li>
+          ))}
         </ul>
       </div>
     </div>
