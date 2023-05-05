bc2d898f2a31f7139417dd102438522935ff9746
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-05-05 10:09:39 +0300

    quiz-app react design p1

---
+++ quiz-app/src/App.jsx
@@ -0,0 +1,20 @@
+import "./app.css"
+
+function App() {
+  return (
+    <div className="app">
+      <div className="main">main</div>
+      <div className="pyramid">
+        <ul className="moneyList">
+          <li className="moneyListItem">$ 500</li>
+          <li className="moneyListItem">$ 400</li>
+          <li className="moneyListItem">$ 300</li>
+          <li className="moneyListItem">$ 200</li>
+          <li className="moneyListItem">$ 100</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
