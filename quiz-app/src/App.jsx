257b3b884997d0de98f1202c976e2fed64e3f70e
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-05-06 11:09:42 +0300

    quiz-app logics p1

--- quiz-app/src/App.jsx
+++ quiz-app/src/App.jsx
@@ -4,6 +4,76 @@ import Trivia from "./components/Trivia";
 
 function App() {
   const [questionNumber, setQuestionNumber] = useState(1);
+  const [timeOut, setTimeOut] = useState(false);
+
+  const data = [
+    {
+      id: 1,
+      question: "Rolex is a company that specializes in what type of product?",
+      answers: [
+        {
+          text: "Phone",
+          correct: false,
+        },
+        {
+          text: "Watches",
+          correct: true,
+        },
+        {
+          text: "Food",
+          correct: false,
+        },
+        {
+          text: "Cosmetic",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 2,
+      question: "When did the website `Facebook` launch?",
+      answers: [
+        {
+          text: "2004",
+          correct: true,
+        },
+        {
+          text: "2005",
+          correct: false,
+        },
+        {
+          text: "2006",
+          correct: false,
+        },
+        {
+          text: "2007",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 3,
+      question: "Who played the character of harry potter in movie?",
+      answers: [
+        {
+          text: "Johnny Deep",
+          correct: false,
+        },
+        {
+          text: "Leonardo Di Caprio",
+          correct: false,
+        },
+        {
+          text: "Denzel Washington",
+          correct: false,
+        },
+        {
+          text: "Daniel Red Cliff",
+          correct: true,
+        },
+      ],
+    },
+  ];
   const moneyPyramid = useMemo(
     () =>
       [
@@ -32,15 +102,28 @@ function App() {
         <div className="top">
           <div className="timer">30</div>
         </div>
-        <div className="bottom"><Trivia/></div>
+        <div className="bottom">
+          <Trivia
+            data={data}
+            setTimeOut={setTimeOut}
+            setQuestionNumber={setQuestionNumber}
+            questionNumber={questionNumber}
+          />
+        </div>
       </div>
       <div className="pyramid">
         <ul className="moneyList">
           {moneyPyramid.map((m) => (
-          <li className={questionNumber === m.id ? "moneyListItem active" : "moneyListItem"}>
-            <span className="moneyListItemNumber">{m.id}</span>
-            <span className="moneyListItemAmount">{m.amount}</span>
-          </li>
+            <li
+              className={
+                questionNumber === m.id
+                  ? "moneyListItem active"
+                  : "moneyListItem"
+              }
+            >
+              <span className="moneyListItemNumber">{m.id}</span>
+              <span className="moneyListItemAmount">{m.amount}</span>
+            </li>
           ))}
         </ul>
       </div>
--- quiz-app/src/components/Trivia.jsx
+++ quiz-app/src/components/Trivia.jsx
@@ -1,14 +1,24 @@
+import { useEffect, useState } from "react";
 
-export default function Trivia() {
+export default function Trivia({
+  data,
+  setTimeOut,
+  setQuestionNumber,
+  questionNumber,
+}) {
+  const [question,setQuestion]=useState(null);
+  useEffect(()=>{
+    setQuestion(data[questionNumber - 1])
+  },[data,questionNumber])
   return (
     <div className="trivia">
-        <div className="question">What's the best football club in Turkey ?</div>
-        <div className="answers">
-            <div className="answer correct">Galatasaray</div>
-            <div className="answer">Beşiktaş</div>
-            <div className="answer">Fenerbahçe</div>
-            <div className="answer">TrabzonSpor</div>
-        </div>
+      <div className="question">What's the best football club in Turkey ?</div>
+      <div className="answers">
+        <div className="answer correct">Galatasaray</div>
+        <div className="answer">Beşiktaş</div>
+        <div className="answer">Fenerbahçe</div>
+        <div className="answer">TrabzonSpor</div>
+      </div>
     </div>
-  )
+  );
 }
