c83d64c7e9d76c0252204d6c6a9a5b73503ddc92
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-05-08 11:47:43 +0300

    quiz-app Sounds works

--- quiz-app/src/components/Trivia.jsx
+++ quiz-app/src/components/Trivia.jsx
@@ -1,4 +1,8 @@
 import { useEffect, useState } from "react";
+import useSound from "use-sound";
+import correct from "../assets/correct.wav";
+import wrong from "../assets/wrong.wav";
+import play from "../assets/play.wav";
 
 export default function Trivia({
   data,
@@ -9,6 +13,13 @@ export default function Trivia({
   const [question, setQuestion] = useState(null);
   const [selectedAnswer, setSelectedAnswer] = useState(null);
   const [className, setClassName] = useState("answer");
+  const [letsPlay] = useSound(play);
+  const [correctAnswer] = useSound(correct);
+  const [wrongAnswer] = useSound(wrong);
+
+  useEffect(() => {
+    letsPlay();
+  }, [letsPlay]);
 
   useEffect(() => {
     setQuestion(data[questionNumber - 1]);
@@ -28,10 +39,16 @@ export default function Trivia({
     });
     delay(5000, () => {
       if (a.correct) {
-        setQuestionNumber((prev) => prev + 1);
-        setSelectedAnswer(null);
+        correctAnswer();
+        delay(1000, () => {
+          setQuestionNumber((prev) => prev + 1);
+          setSelectedAnswer(null);
+        });
       } else {
-        setStop(true);
+        wrongAnswer();
+        delay(1000, () => {
+          setStop(true);
+        });
       }
     });
   };
